66866a6841ee720beb01194fd435a0f5e867499c
Brian Pei <brianpei01@users.noreply.github.com>
2016-08-08 03:22:26 -0400

    Create server.R

---
+++ shiny/server.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(leaflet)
+Carecenterbyregion <- read.csv("//mac/Home/Desktop/R_repo/Carecenterbyregion.csv")
+shinyServer(function(input, output, session){
+  
+  lats <- reactive({
+    a <- subset(Carecenterbyregion, Carecenterbyregion$Primary.Specialty %in% input$specialty)
+    lats <- a$Latitude
+    return(lats)
+  })
+  
+  longs <- reactive({
+    a <- subset(Carecenterbyregion, Carecenterbyregion$Primary.Specialty %in% input$specialty)
+    longs <- a$Longitude
+    return(longs)
+  })
+  
+  newlabels <- reactive({
+    a <- subset(Carecenterbyregion, Carecenterbyregion$Primary.Specialty %in% input$specialty)
+    newlabels <- paste(a$Account.Name, a$Primary.Specialty, sep=", ")
+    return(newlabels)
+  })
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = -77.17862, lat = 38.93652, zoom = 10) %>%
+      addMarkers(lng = longs(), lat = lats(), popup=newlabels())
+  })
+})
